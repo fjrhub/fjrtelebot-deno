6190fb98a38f06a962c992ce38f0b7249ff51070
fjrhub <muhammadfajrulfalah@proton.me>
2026-03-26 02:55:02 +0700

    refactor(handler): update start command trigger from 'halo' to 'ai'

--- handler.js
+++ handler.js
@@ -1,12 +1,12 @@
 import ping from "./commands/ping.js";
-import halo from "./commands/halo.js";
+import ai from "./commands/ai.js";
 import start from "./commands/start.js";
 import history from "./commands/history.js";
 import clearhistory from "./commands/clearhistory.js";
 
 export const registerHandlers = (bot) => {
   ping(bot);
-  halo(bot);
+  ai(bot);
   start(bot);
   history(bot);
   clearhistory(bot);
