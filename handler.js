2cef736f4dc55194f43481bb968db147bed3bb8c
fjrhub <muhammadfajrulfalah@proton.me>
2026-03-26 00:58:22 +0700

    refactor(handler): register new commands (halo, history, clearhistory)

--- handler.js
+++ handler.js
@@ -1,9 +1,13 @@
 import ping from "./commands/ping.js";
 import halo from "./commands/halo.js";
 import start from "./commands/start.js";
+import history from "./commands/history.js";
+import clearhistory from "./commands/clearhistory.js";
 
 export const registerHandlers = (bot) => {
   ping(bot);
   halo(bot);
   start(bot);
+  history(bot);
+  clearhistory(bot);
 };
