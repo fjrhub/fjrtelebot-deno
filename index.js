d0eae44283bfae890ad71b4130de3ba96beff7e8
fjrhub <muhammadfajrulfalah@proton.me>
2026-03-25 23:27:38 +0700

    feat(webhook): setup webhook handler for production

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 import "jsr:@std/dotenv/load";
-import { Bot, webhookCallback } from "npm:grammy";
+import { webhookCallback } from "npm:grammy";
+import { bot } from "./bot.js";
+import { registerHandlers } from "./handler.js";
 
-const TOKEN = Deno.env.get("TOKEN");
-const URL = Deno.env.get("URL"); // tambahkan ini di environment kamu (URL publik)
-
-const bot = new Bot(TOKEN);
-
-bot.command("ping", (ctx) => ctx.reply("🏓 Pong!"));
-bot.command("halo", (ctx) => ctx.reply("Halo juga!"));
-bot.command("start", (ctx) => ctx.reply("Bot aktif dengan webhook 🚀"));
-
-// set webhook otomatis (sekali jalan)
-await fetch(`https://api.telegram.org/bot${TOKEN}/setWebhook?url=${URL}`);
+registerHandlers(bot);
 
 const handleUpdate = webhookCallback(bot, "std/http");
+
 Deno.serve(handleUpdate);
