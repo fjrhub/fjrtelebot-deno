25ca4bb5bbc9feaf142920cca20b3d25b59fa905
fjrhub <muhammadfajrulfalah@proton.me>
2026-03-28 00:38:30 +0700

    revert: simplify webhook handler to default Deno.serve

--- index.js
+++ index.js
@@ -7,19 +7,4 @@ registerHandlers(bot);
 
 const handleUpdate = webhookCallback(bot, "std/http");
 
-Deno.serve((req) => {
-  const url = new URL(req.url);
-
-  // hanya POST ke "/" yang diproses
-  if (url.pathname === "/" && req.method === "POST") {
-    return handleUpdate(req);
-  }
-
-  // favicon biar gak error di log
-  if (url.pathname === "/favicon.ico") {
-    return new Response(null, { status: 204 });
-  }
-
-  // selain itu langsung ditolak
-  return new Response(null, { status: 404 });
-});
+Deno.serve(handleUpdate);
