721a631fd6c0d57db9e811af9e4e424c20899e8d
fjrhub <muhammadfajrulfalah@proton.me>
2026-04-29 10:33:01 +0700

    feat(commands): disable img command on deno

--- commands/tools/img.js
+++ commands/tools/img.js
@@ -1,37 +1,37 @@
-import { InputFile } from "npm:grammy";
+// import { InputFile } from "npm:grammy";
 
-export default (bot) => {
-  bot.command("img", async (ctx) => {
-    try {
-      const text = ctx.message.text.trim();
-      const args = text.split(" ").slice(1);
+// export default (bot) => {
+//   bot.command("img", async (ctx) => {
+//     try {
+//       const text = ctx.message.text.trim();
+//       const args = text.split(" ").slice(1);
 
-      if (!args.length) {
-        return;
-      }
+//       if (!args.length) {
+//         return;
+//       }
 
-      const url = args[0];
+//       const url = args[0];
 
-      try {
-        new URL(url);
-      } catch {
-        return;
-      }
+//       try {
+//         new URL(url);
+//       } catch {
+//         return;
+//       }
 
-      const res = await fetch(url);
+//       const res = await fetch(url);
 
-      if (!res.ok) {
-        return;
-      }
+//       if (!res.ok) {
+//         return;
+//       }
 
-      const bytes = new Uint8Array(await res.arrayBuffer());
+//       const bytes = new Uint8Array(await res.arrayBuffer());
 
-      await ctx.replyWithPhoto(new InputFile(bytes, "image.png"));
+//       await ctx.replyWithPhoto(new InputFile(bytes, "image.png"));
 
-      // hapus pesan command pengirim
-      await ctx.deleteMessage().catch(() => {});
-    } catch (err) {
-      console.error(err);
-    }
-  });
-};
+//       // hapus pesan command pengirim
+//       await ctx.deleteMessage().catch(() => {});
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   });
+// };
