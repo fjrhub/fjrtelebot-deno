4af15ab6a630f425ad27dc92033261e817412974
fjrhub <muhammadfajrulfalah@proton.me>
2026-06-09 16:43:54 +0700

    fix(kv): manually nuked corrupted KV database via dashboard to resolve deserialization crash

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ import "jsr:@std/dotenv/load";
 import { webhookCallback } from "npm:grammy";
 import { bot } from "./bot.js";
 import { registerHandlers } from "./handler.js";
-import { registerCrons } from "./cron/index.js";
 
 registerHandlers(bot);
-registerCrons(bot);
 
 const handleUpdate = webhookCallback(bot, "std/http");
 
